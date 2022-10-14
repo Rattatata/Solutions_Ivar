4141fbe329b8b4b2b4dbcd141bb8fee91bb1536a
Ivar Holmlund <ivholmlu@nmbu.no>
2022-10-14 09:43:44 +0200

    Create Janitor_Troubles.py

---
+++ Janitor_Troubles.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 14 09:39:14 2022
+
+@author: Ivar
+"""
+#https://open.kattis.com/problems/janitortroubles
+
+import math as m
+
+
+a, b, c, d = [int(i) for i in input().split()]
+
+
+s = (a+b+c+d)/2
+
+K = m.sqrt((s-a)*(s-b)*(s-c)*(s-d))
+
+print(K)
