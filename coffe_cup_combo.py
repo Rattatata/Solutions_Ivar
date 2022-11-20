9af3ebb2306553c011ec1a2c140583b1179cb7dd
Ivar Holmlund <ivholmlu@nmbu.no>
2022-11-20 15:38:50 +0100

    Added solution for coffe cup combo

---
+++ coffe_cup_combo.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 20 15:22:56 2022
+
+@author: Ivar
+https://open.kattis.com/problems/coffeecupcombo
+"""
+
+N = int(input())
+
+
+coffeindex = input()
+
+counter = 0
+coffees = 0
+prev_coffe = 0
+
+for idx in coffeindex:
+    
+    if idx == '0':
+        coffees += -1
+    else: 
+        coffees = 3
+    
+    if coffees > 0:
+        counter += 1
+    
+print(counter)
+    
+    
+        
+    
+    
