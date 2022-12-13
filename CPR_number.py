5680295f36b503a8076bf1ea9badba4780dbc535
Ivar Holmlund <ivholmlu@nmbu.no>
2022-12-13 18:04:00 +0100

    Changed variable name

--- CPR_number.py
+++ CPR_number.py
@@ -5,11 +5,11 @@ Created on Tue Dec 13 17:49:57 2022
 @author: Ivar
 """
 
-inn = input()
-inn = inn.replace('-', '')
+CPR = input()
+CPR = CPR.replace('-', '')
 scalars = [4,3,2,7,6,5,4,3, 2, 1]
 value = 0
-for val, scal in zip(inn, scalars):
+for val, scal in zip(CPR, scalars):
     value += int(val)*scal
 
 if value % 11 == 0:
